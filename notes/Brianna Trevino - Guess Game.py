6a11ea05c558c413c784ba6e126c702a8fb883bd
6u2b <44374433+6u2b@users.noreply.github.com>
2018-11-02 09:45:51 -0700

    nov.2

--- notes/Brianna Trevino - Guess Game.py
+++ notes/Brianna Trevino - Guess Game.py
@@ -1,6 +1,16 @@
-maximum = 10
-minimum = 1
-num = 3
+numer = 3
+guesses = 5
+while guesses > 0:
+    num = int(input("What's a number from 1 to 10"))
+    if num >= 4:
+        print("Too high")
+        guesses = guesses -1
+    elif num <= 2:
+        print("a little higher")
+    elif num == 3:
+        print("Correct")
+        guesses = 0
 
-
-
+print()
+if guesses == 0:
+    print("Game over")
--- notes/Hello world.py
+++ notes/Hello world.py
@@ -121,13 +121,12 @@ if sunny:
 
 
 def grade_calc(percentage):
-    if percentage >= 90:
+    eli if percentage >= 90:
         return "A"
     elif percentage >= 80:
         return "B"
     elif percentage >= 70:
-        return "C"
-    elif percentage >= 60:
+       f percentage >= 60:
         return "D"
     else:
         return "F"
