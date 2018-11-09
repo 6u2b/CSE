372c04cd949b8611e0e7b6f2361e3147e43a71c3
6u2b <44374433+6u2b@users.noreply.github.com>
2018-11-09 10:37:33 -0800

    nov.9

--- notes/lucky 7s.py
+++ notes/lucky 7s.py
@@ -2,7 +2,15 @@ import random
 money = 15
 dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 6)
-
-if dice1 + dice2 == 7:
-    print("You rolled a 7!")
-    print("your new balance is")
+lose = True
+while money > 0:
+    num = 1
+    money = money - num
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    if dice1 + dice2 == 7:
+        print("You rolled a 7!")
+        print("your new balance is %s" % money)
+        print("+ %s, + 4" % num)
+    elif not dice1 + dice2 == 7:
+        print("You got %s and %s, - %s" % (dice1, dice2, num))
