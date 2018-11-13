bfd3117a5ec4a544214f4fd4dbbfafb3600aaad3
6u2b <44374433+6u2b@users.noreply.github.com>
2018-11-13 09:45:13 -0800

    nov.13

--- notes/lucky 7s.py
+++ notes/lucky 7s.py
@@ -3,14 +3,24 @@ money = 15
 dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 6)
 lose = True
+round_ = 1
+highscore_ = 0
+best_round = 0
 while money > 0:
+    print("Round %s" % round_)
     num = 1
     money = money - num
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     if dice1 + dice2 == 7:
         print("You rolled a 7!")
-        print("your new balance is %s" % money)
         print("+ %s, + 4" % num)
     elif not dice1 + dice2 == 7:
         print("You got %s and %s, - %s" % (dice1, dice2, num))
+        money = money + num
+    if highscore_ < money:
+        highscore_ = money
+        best_round = round_
+    print("your new balance is %s" % money)
+    round_ = round_ + 1
+    print()
