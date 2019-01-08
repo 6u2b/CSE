1064564d9efe23a73e9cbd4f67cf37d53687e1ff
6u2b <44374433+6u2b@users.noreply.github.com>
2019-01-08 09:44:46 -0800

    ah

---
+++ Semester Review.py
@@ -0,0 +1,22 @@
+print("Hello world")
+
+# single line comment
+cars = 5
+driving = True
+
+print("I have %d cars" % cars)
+
+age = input("How old are you?")
+print(age + "??? You really shouldn't lie about your age.")
+
+colors = ["blue", "red", "yellow", "green", "cyan"]
+
+colors.append("blue")
+colors.pop(0)
+print(colors)
+print(colors[2])
+
+
+len(colors)
+print(colors)
+
