2580e3fdf89519328641b15763cabaeb7fcd02c3
6u2b <44374433+6u2b@users.noreply.github.com>
2019-04-12 10:39:08 -0700

    4.12

---
+++ ITEMS - Brianna Trevino.py
@@ -0,0 +1,79 @@
+class Item(object):
+    def __init__(self, name):
+        self.name = name
+
+
+class Beaker(Item):
+    def __init__(self):
+        super(Beaker, self) .__init__("Beaker")
+
+
+class Scale(Item):
+    def __init__(self):
+        super(Scale, self) .__init__("Dragon Scale")
+
+
+class Leg(Item):
+    def __init__(self):
+        super(Leg, self) .__init__("Frog Leg")
+
+
+class Sunflower(Item):
+    def __init__(self):
+        super(Sunflower, self) .__init__("Sunflower")
+
+
+class Watch(Item):
+    def __init__(self):
+        super(Watch, self) .__init__("Watch")
+
+
+class Seed(Item):
+    def __init__(self):
+        super(Seed, self) .__init__("Seed")
+
+
+class Scissors (Item):
+    def __init__(self):
+        super(Scissors, self) .__init__("Scissors")
+
+
+class Knife(Item):
+    def __init__(self):
+        super(Knife, self) .__init__("Knife")
+
+
+class Jar(Item):
+    def __init__(self):
+        super(Jar, self) .__init__("Bean jar")
+
+
+class Shield(Item):
+    def __init__(self):
+        super(Shield, self) .__init__("Shield")
+
+
+class Leash(Item):
+    def __init__(self):
+        super(Leash, self) .__init__("Leash")
+
+
+class Collar(Item):
+    def __init__(self):
+        super(Collar, self) .__init__("Collar")
+
+
+class Shears(Item):
+    def __init__(self):
+        super(Shears, self) .__init__("Shears")
+
+
+class Bone(Item):
+    def __init__(self):
+        super(Bone, self) .__init__("Bone")
+
+
+class Lettuce(Item):
+    def __init__(self):
+        super(Lettuce, self) .__init__("Lettuce")
+
